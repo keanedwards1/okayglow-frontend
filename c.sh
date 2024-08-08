653743953ce95de5eefa39257e5791a8d0c367a7
keanedwards1 <112280196+keanedwards1@users.noreply.github.com>
2024-08-07 23:54:31 -0700

    small commit

---
+++ c.sh
@@ -0,0 +1,3 @@
+git add .
+git commit -m "small commit"
+git push origin main
--- utils/trpc.ts
+++ utils/trpc.ts
@@ -1,15 +1,18 @@
+// utils/trpc.ts
+
 import { createTRPCNext } from '@trpc/next';
 import { httpBatchLink } from '@trpc/client';
 import type { AppRouter } from '../../shared/src';
 
 export const trpc = createTRPCNext<AppRouter>({
     config() {
+        const url = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3001/trpc';
         return {
             links: [
                 httpBatchLink({
-                    url: 'http://localhost:3001/trpc'
+                    url,
                 })
             ]
         }
     }
-})
+});
